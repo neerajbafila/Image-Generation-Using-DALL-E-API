5c6fc382148b6e4667f5c9f98e4b63e4b962b373
neerajbafila@gmail.com <neerajbafila@gmail.com>
2023-06-07 19:37:40 +0530

    image variation added

--- src/page_3.py
+++ src/page_3.py
@@ -1,2 +1,31 @@
-def page_thee():
-    pass
+import streamlit as st
+from src.utils.utils import resize_image, get_width_height
+from PIL import Image
+import openai
+
+def page_three():
+    st.title("OpenAI DALL·E Image Variation")
+    st.info("""#### NOTE: you can download image by \
+    right clicking on the image and select save image as option""")
+    
+    with st.form(key="form"):
+        uploaded_file = st.file_uploader(label='upload file', type=['png', 'jpeg'])
+        size = st.selectbox('select size of image', ('256x256', '512x512', '1024x1024'))
+        num_images = st.selectbox('enter the number of images to be generated', (1,2,3,4))
+
+        submit = st.form_submit_button(label='submit')
+    if submit:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="uploaded image", use_column_width=True)
+        width, height =  get_width_height(size)
+        resize_img =  resize_image(image, width, height)
+        response = openai.Image.create_variation(image=resize_img, n=num_images, size=size)
+
+        for idx in range(num_images):
+            img_url = response['data'][idx]['url']
+
+            st.image(img_url, caption=f'generated image {idx+1}', use_column_width=True)
+            
+
+
+
